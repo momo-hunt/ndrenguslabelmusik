77884a19f11fe306dcd793304ea8622c0769435c
momo-hunt <momohunt8@gmail.com>
2023-04-23 23:22:41 +0700

    test ukuran

--- src/routes/(dashboard)/test/+page.svelte
+++ src/routes/(dashboard)/test/+page.svelte
@@ -1,35 +1,47 @@
-<main>
-	<div class="a1">A</div>
-	<div class="a2">B</div>
-	<div class="a3">C</div>
-	<div class="a4">D</div>
-	<div class="a5">E</div>
-</main>
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
 
-<style>
-	main {
-		display: flex;
-		flex-direction: column;
-		gap: 2rem;
-	}
+<p>Paragraf</p>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, molestias?</p>
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio veritatis voluptatem ullam, ipsam
+	ratione, deserunt vero dicta ipsum explicabo quod placeat. Alias obcaecati aliquam, aut blanditiis
+	voluptate suscipit vitae rem ipsa vero temporibus similique in, doloribus, voluptatem culpa quae
+	dolores. Illum vel assumenda omnis quia nam natus non consectetur quaerat.
+</p>
+<small>Small</small>
+<a href="/test">Link</a>
+<button>Button</button>
+<label for="">Label</label>
+<input type="text" placeholder="input" />
 
-	main > div {
-		background-color: rgb(64, 55, 145);
-	}
+<input type="radio" name="" id="" /><label for="">radio</label>
 
-	main > div.a1 {
-		width: 360px;
-	}
-	main > div.a2 {
-		width: 420px;
-	}
-	main > div.a3 {
-		width: 540px;
-	}
-	main > div.a4 {
-		width: 640px;
-	}
-	main > div.a5 {
-		width: 720px;
-	}
-</style>
+<input type="button" value="button inpt" />
+
+<input type="checkbox" name="" id="" /><label for="">cek</label>
+
+<input type="color" name="" id="" />
+<input type="date" name="" id="" />
+<input type="datetime" name="" id="" />
+<input type="datetime-local" name="" id="" />
+<input type="email" name="" id="" />
+<input type="time" name="" id="" />
+<input type="url" name="" id="" />
+
+<ul>
+	<li>asd</li>
+	<li>asd</li>
+	<li>asd</li>
+</ul>
+
+<ol>
+	<li>ol</li>
+	<li>ol</li>
+	<li>ol</li>
+	<li>ol</li>
+</ol>
