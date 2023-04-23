c808f2963480194f6a736db566ace1269f2d5910
momo-hunt <momohunt8@gmail.com>
2023-04-23 23:51:39 +0700

    fix login interface

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -13,7 +13,7 @@
 <main>
 	<img src={logo} alt="Ndrengus Label Musik" />
 
-	<p>LOGIN</p>
+	<h2>LOGIN</h2>
 
 	<form
 		method="post"
@@ -28,15 +28,15 @@
 		}}
 	>
 		<label for="username">
-			<Icon src={User} solid size="50" />
+			<Icon src={User} solid size="24" />
 			<input type="text" name="username" id="username" placeholder="Username" />
 		</label>
 		<label for="password">
-			<Icon src={LockClosed} solid size="50" />
+			<Icon src={LockClosed} solid size="24" />
 
 			<input type={typeInputPassword} name="password" id="password" placeholder="Password" />
 			<button type="button" on:click={showPassword}>
-				<Icon src={typeInputPassword == 'password' ? EyeSlash : Eye} solid size="50" />
+				<Icon src={typeInputPassword == 'password' ? EyeSlash : Eye} solid size="24" />
 			</button>
 		</label>
 		<button type="submit">LOGIN</button>
@@ -46,81 +46,48 @@
 
 <style>
 	main {
-		margin-top: 150px;
-		color: #ff9000;
-		font-size: 30px;
+		margin-top: 56px;
 		text-align: center;
-	}
-
-	main > p {
-		margin: 4rem 0;
-		font-weight: 500;
 	}
 
 	img {
-		width: 157px;
-		aspect-ratio: 1/1;
+		width: 20%;
 	}
 
 	form {
-		padding: 0 2rem;
+		padding: 2rem;
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		gap: 2rem;
-	}
-
-	label {
-		width: 100%;
-		background-color: var(--bg-2);
-		display: flex;
-		align-items: center;
-		border-radius: 4rem;
-		padding: 3rem;
 		gap: 1rem;
 	}
 
-	input {
-		text-align: center;
+	form > label,
+	form > button {
+		background-color: var(--bg-2);
+		padding: 0 2rem;
+		border-radius: 2rem;
+	}
+
+	form > button {
+		background-color: var(--bg-4);
+		font-weight: 600;
+	}
+
+	form > label,
+	input,
+	input::placeholder {
+		color: var(--bg-1);
+	}
+
+	input::placeholder {
+		opacity: 0.6;
 	}
 
 	input:focus::placeholder {
 		color: transparent;
 	}
 
-	input#username {
-		padding-right: 50px;
-	}
-
-	input,
-	input::placeholder,
-	form > button,
-	label {
-		color: var(--bg-1);
-	}
-
-	input,
-	form > button {
-		font-size: inherit;
-	}
-
-	form > button {
-		width: 60%;
-		font-size: 40px;
-		font-weight: 600;
-		padding: 2rem;
-		border-radius: 4rem;
-		background-color: var(--bg-3);
-	}
-
-	a {
-		font-weight: 300;
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-
-	form label button {
-		width: fit-content;
+	button[type='button'] {
+		padding: 0;
 	}
 </style>
