19678d0e4fe9023944e0ad78c746bf479c7d2215
momo-hunt <momohunt8@gmail.com>
2023-04-23 22:06:30 +0700

    test ukuran

---
+++ src/routes/(dashboard)/test/+page.svelte
@@ -0,0 +1,35 @@
+<main>
+	<div class="a1">A</div>
+	<div class="a2">B</div>
+	<div class="a3">C</div>
+	<div class="a4">D</div>
+	<div class="a5">E</div>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+
+	main > div {
+		background-color: rgb(64, 55, 145);
+	}
+
+	main > div.a1 {
+		width: 360px;
+	}
+	main > div.a2 {
+		width: 420px;
+	}
+	main > div.a3 {
+		width: 540px;
+	}
+	main > div.a4 {
+		width: 640px;
+	}
+	main > div.a5 {
+		width: 720px;
+	}
+</style>
