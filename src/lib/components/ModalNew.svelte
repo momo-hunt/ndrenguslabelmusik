fac7ddb9bc992fa43f805700a9ea929192d201e4
momo-hunt <momohunt8@gmail.com>
2023-04-29 12:20:57 +0700

    buat form pendapatan

---
+++ src/lib/components/ModalNew.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import { Icon, XCircle } from 'svelte-hero-icons';
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<main>
+	<button class="backdrop" on:click={() => dispatch('close', null)}>&nbsp;</button>
+	<section>
+		<button type="button" class="close" on:click={() => dispatch('close', null)}>
+			<Icon src={XCircle} size="32" />
+		</button>
+		<slot />
+	</section>
+</main>
+
+<style>
+	/* * {
+		border: var(--border);
+	} */
+
+	main {
+		position: fixed;
+		z-index: 99;
+		top: 0;
+		left: 0;
+		width: 100%;
+		min-height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: start;
+		padding: 4rem 2rem;
+		background-color: rgba(0, 0, 0, 0.5);
+	}
+
+	main > button.backdrop {
+		cursor: default;
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		padding: 0;
+	}
+
+	main section > button.close {
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: 0.5rem;
+	}
+
+	main section {
+		position: relative;
+		background-color: var(--bg-2);
+		color: var(--bg-1);
+		width: 100%;
+		padding: 1rem;
+		border-radius: 1rem;
+	}
+</style>
--- src/routes/(v1)/(app)/+page.svelte
+++ src/routes/(v1)/(app)/+page.svelte
@@ -1,10 +1,174 @@
 <script>
+	import ModalNew from '$lib/components/ModalNew.svelte';
 	import InfoPenarikan from './InfoPenarikan.svelte';
 	import Welcome from './Welcome.svelte';
+	import {
+		ArrowDownCircle,
+		ArrowUpCircle,
+		CheckCircle,
+		ChevronDown,
+		Icon,
+		Plus
+	} from 'svelte-hero-icons';
 
 	export let data;
+
+	let dataModalTambah = null;
 </script>
 
 <Welcome username={data?.user?.name || 'null'} />
 
-<InfoPenarikan />
+{#if data?.user?.role == 'admin'}
+	{#if dataModalTambah}
+		<ModalNew>Tambah</ModalNew>
+	{/if}
+
+	<section>
+		<header>
+			<h3>Rafa</h3>
+			<span><Icon src={ChevronDown} size="24" solid /></span>
+		</header>
+		<article>
+			<header class="green">
+				<span><Icon src={ArrowUpCircle} solid size="24" /></span>
+				<h4>Pendapatan</h4>
+				<h3>$ 2.0</h3>
+			</header>
+			<ul>
+				<li>
+					<div>April</div>
+					<div>$ 1.0</div>
+				</li>
+				<li>
+					<div>Mei</div>
+					<div>$ 1.0</div>
+				</li>
+			</ul>
+			<button>
+				<Icon src={Plus} size="24" solid />
+				<span>Tambah</span>
+			</button>
+		</article>
+
+		<article>
+			<header class="red">
+				<span><Icon src={ArrowDownCircle} solid size="24" /></span>
+				<h4>Penarikan</h4>
+				<h3>$ 1.0</h3>
+			</header>
+			<ul>
+				<li>
+					<div>Mei</div>
+					<div>$ 1.0</div>
+				</li>
+			</ul>
+		</article>
+		<article>
+			<header class="blue">
+				<span><Icon src={CheckCircle} solid size="24" /></span>
+				<h4>Saldo</h4>
+				<h3>$ 1.0</h3>
+			</header>
+		</article>
+	</section>
+{/if}
+
+{#if data?.user?.role != 'admin'}
+	<InfoPenarikan />
+{/if}
+
+<style>
+	* {
+		/* border: var(--border); */
+	}
+
+	section {
+		margin: 0.5rem 1rem;
+		background-color: var(--bg-2);
+		color: var(--bg-1);
+		padding: 1rem;
+		border-radius: 1rem;
+	}
+
+	section > header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	section > header > span {
+		opacity: 0.6;
+	}
+
+	section article > button {
+		gap: 0.5rem;
+		background-color: var(--bg-2);
+		padding: 0.5rem 1rem;
+		margin-top: 0.5rem;
+		border-radius: 0.5rem;
+		outline: 2px dashed rgba(0, 0, 0, 0.2);
+		margin-left: 50%;
+		transform: translate(-50%);
+	}
+
+	section article > button:hover {
+		transition: outline 0.3s ease;
+		outline-color: var(--bg-1);
+	}
+
+	section article {
+		background-color: var(--bg-4);
+		margin: 0.5rem 0;
+		padding: 1rem;
+		border-radius: 1rem;
+	}
+
+	section article header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border-bottom: 1px solid var(--bg-1);
+		gap: 0.5rem;
+	}
+
+	section article header.blue {
+		border: none;
+	}
+
+	section article header > span {
+		height: 1.5rem;
+		opacity: 0.6;
+	}
+	section article header.green > span {
+		color: rgb(15, 160, 15);
+	}
+
+	section article header.red > span {
+		color: rgb(160, 15, 15);
+	}
+
+	section article header.blue > span {
+		color: rgb(15, 15, 160);
+	}
+
+	section article header h3 {
+		color: var(--bg-3);
+		margin-left: auto;
+	}
+
+	section article ul {
+		margin: 0;
+		padding: 0 0.5rem;
+	}
+
+	section article ul li {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+	}
+
+	section article ul li:last-child {
+		border-bottom: none;
+	}
+</style>
